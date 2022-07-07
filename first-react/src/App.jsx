cad7939c27b389b9e6d8f9c390016de1c3f7f46f
Chuck Breter <chuckb0384@aim.com>
2022-07-07 14:51:12 -0400

    first-jsx

---
+++ first-react/src/App.jsx
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+
+      <table>
+        <tr>
+          <th>Things I need to do:</th>
+        </tr>
+        <tr>
+          <td>Learn React</td>
+        </tr>
+        <tr>
+          <td>Climb Mt. Everest</td>
+        </tr>
+        <tr>
+          <td>Run a Marathon</td>
+        </tr>
+        <tr>
+          <td>Feed the dogs</td>
+        </tr>
+
+      </table>
+    
+    </div>
+  );
+}
+
+export default App;
